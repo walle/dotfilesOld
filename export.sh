086ec3319f304e38fd00bfd270d7cce0134827c2
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-09-03 21:18:13 +0200

    Add watchr script to export

--- export.sh
+++ export.sh
@@ -2,6 +2,6 @@
 
 USER_HOME=/home/$(whoami)/
 
-cp -v ./.bash* ./.gitconfig ./.gitignore ./.gemrc $USER_HOME
+cp -v ./.bash* ./.gitconfig ./.gitignore ./.gemrc ./.watchr $USER_HOME
 
 source $USER_HOME/.bashrc
